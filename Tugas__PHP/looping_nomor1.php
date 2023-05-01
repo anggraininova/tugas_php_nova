92f3c1da8e6fff30fd5e193a7f9618a0ef0b183b
anggraininova <105601196+anggraininova@users.noreply.github.com>
2023-05-01 22:47:12 +0700

    Delete Tugas__PHP directory

--- Tugas__PHP/looping_nomor1.php
+++
@@ -1,9 +0,0 @@
-<?php
-for($a=1; $a<=1; $a++){
-    for($b=1;$b<=10;$b++){
-        $total = $a*$b;
-        echo "<p> $a x $b = $total";
-    }
-    echo "<br>";
-}
-?>
--- Tugas__PHP/looping_nomor2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$star=9;
-for($a=1; $a<=$star; $a++){
-    for($c=$star; $c>=$a; $c-=1){
-        echo "*";
-    }
-    echo "<br>";
-}
-?>
